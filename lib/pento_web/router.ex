5813f276a40a3e3551d7584291ef4e41e6121422
Gavin Henry <ghenry@surevoip.co.uk>
2022-10-07 15:09:18 +0100

    p212

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -99,6 +99,8 @@ defmodule PentoWeb.Router do
       live "/products/:id/edit", ProductLive.Index, :edit
       live "/products/:id", ProductLive.Show, :show
       live "/products/:id/show/edit", ProductLive.Show, :edit
+
+      live "/admin-dashboard", Admin.DashboardLive
     end
 
     get "/users/settings", UserSettingsController, :edit
