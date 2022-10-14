934e927690748553cef2057c585fc92b39d6f8fe
Gavin Henry <ghenry@surevoip.co.uk>
2022-10-14 19:47:55 +0100

    Up to p233

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -7,17 +7,42 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     {:ok,
     socket
     |> assign(assigns)
+    |> assign_age_group_filter()
     |> assign_products_with_average_ratings()
     |> assign_dataset()
     |> assign_chart()
     |> assign_chart_svg()}
   end
 
-  defp assign_products_with_average_ratings(socket) do
+  def handle_event("age_group_filter", %{"age_group_filter" => age_group_filter}, socket) do
+    {:noreply,
+      socket
+      |> assign_age_group_filter(age_group_filter)
+      |> assign_products_with_average_ratings()
+      |> assign_dataset()
+      |> assign_chart()
+      |> assign_chart_svg()}
+  end
+
+  def assign_age_group_filter(socket, age_group_filter) do
+    assign(socket, :age_group_filter, age_group_filter)
+  end
+
+  def assign_age_group_filter(socket) do
     socket
-    |> assign(
-         :products_with_average_ratings,
-         Catalog.products_with_average_ratings())
+    |> assign(:age_group_filter, "all")
+  end
+
+  defp assign_products_with_average_ratings(
+         %{assigns: %{age_group_filter: age_group_filter}} =
+           socket) do
+    assign(
+      socket,
+      :products_with_average_ratings,
+      Catalog.products_with_average_ratings(
+        %{age_group_filter: age_group_filter}
+      )
+    )
   end
 
   def assign_dataset(
